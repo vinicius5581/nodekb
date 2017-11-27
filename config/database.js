090c0cc3bc0ad0a90db1b508fb0c78e1acef8da8
Vinicius Santana <santanaviny@gmail.com>
2017-11-27 03:35:03 -0800

    Configured database for heroku

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
+const databaseUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/nodekb';
+
 module.exports = {
-  database: 'mongodb://localhost:27017/nodekb',
+  database: databaseUrl,
   secret: 'yoursecret'
 }
